739d48a6b553fac09d4f5f821e792d855554ba29
brenorodriguesdev <brenorodriguesdev@gmail.com>
2024-12-14 01:03:53 -0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,21 @@
 import React, { useEffect } from 'react';
 import Hls from 'hls.js';
 
+const serverHLS = String(process.env.NEXT_PUBLIC_HLS)
+
 export default function Home() {
   useEffect(() => {
     const video = document.getElementById('video') as HTMLVideoElement;
 
     if (Hls.isSupported()) {
       const hls = new Hls();
-      hls.loadSource('http://18.215.183.34/hls/stream.m3u8'); // Coloque a URL do seu arquivo m3u8 aqui
+      hls.loadSource(serverHLS); // Coloque a URL do seu arquivo m3u8 aqui
       hls.attachMedia(video);
       hls.on(Hls.Events.MANIFEST_PARSED, () => {
         video.play();
       });
     } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-      video.src = 'http://18.215.183.34/hls/stream.m3u8';
+      video.src = serverHLS;
       video.addEventListener('loadedmetadata', () => {
         video.play();
       });
